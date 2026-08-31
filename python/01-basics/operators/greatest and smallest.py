98b2eb5151e8aa9ad77518c6fd37b26569040cd7
Naveen <naveennimmanagoti59@gmail.com>
2026-08-31 21:12:39 +0530

    program greatest and smallest

---
+++ python/01-basics/operators/greatest and smallest.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a < b:
+    print("Smaller:", a)
+    print("Larger:", b)
+else:
+    print("Smaller:", b)
+    print("Larger:", a)
