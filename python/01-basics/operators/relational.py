a558799cb3d36e61356d92e4a937b032ae5fe6b9
Naveen <naveennimmanagoti59@gmail.com>
2026-08-31 20:44:55 +0530

    program for relational operators

---
+++ python/01-basics/operators/relational.py
@@ -0,0 +1,7 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Less than:", a < b)
+print("Less than or equal to:", a <= b)
+print("Greater than:", a > b)
+print("Greater than or equal to:", a >= b)
