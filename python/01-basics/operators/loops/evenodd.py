6ab871fecce548e4c007a8ffde0a65cf22568ce1
Naveen <naveennimmanagoti59@gmail.com>
2026-09-01 10:27:16 +0530

    program for even odd

---
+++ python/01-basics/operators/loops/evenodd.py
@@ -0,0 +1,9 @@
+print("Even numbers:")
+for i in range(1, 51):
+    if i % 2 == 0:
+        print(i, end=" ")
+
+print("\nOdd numbers:")
+for i in range(1, 51):
+    if i % 2 != 0:
+        print(i, end=" ")
